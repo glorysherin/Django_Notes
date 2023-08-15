a96cdabab5ee0ac90cd3fb6fe1183134535bf802
glorysherin <glorysherin22@gmail.com>
2023-08-15 21:37:25 +0530

    urls_views_updated

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from members import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('members/', views.members, name='members'),
 ]
